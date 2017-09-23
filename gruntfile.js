b18f7c5cbda582aad754944f2ffa523840f38e36
Prashanth Sitikond <taurus2819@gmail.com>
2017-09-23 21:22:56 +1200

    second commit - gruntfile.js created

---
+++ gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        concat: {
+            dist: {
+                src: ['components/scripts/*.js'],
+                dest: 'builds/development/js/script.js'
+            }
+        }
+
+    }); //initConfig
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+}; //wrapper function
