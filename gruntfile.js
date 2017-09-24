e26d81a2177054efeb4894c27fb9593d0c8c8d19
Prashanth Sitikond <taurus2819@gmail.com>
2017-09-24 15:12:19 +1300

    branch gwf2 - added sass plugins

--- gruntfile.js
+++ gruntfile.js
@@ -2,13 +2,31 @@ module.exports = function(grunt) {
     grunt.initConfig({
 
         concat: {
+            options: {
+                separator: '\n\n//--------------------------------------\n',
+                banner: '\n\n//--------------------------------------\n',
+            },
             dist: {
                 src: ['components/scripts/*.js'],
                 dest: 'builds/development/js/script.js'
             }
-        }
+        },  //concat
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files : [{
+                    src: 'components/sass/style.scss',
+                    dest: 'builds/development/css/style.css'
+                }]
+            }
+        }   //sass
 
     }); //initConfig
 
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['concat', 'sass']);
 }; //wrapper function
